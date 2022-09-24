9e8d893190bfea836c2b494fbfc7b7c8596aa2fc
willianprietomunoz <willian.prietom@gmail.com>
2022-09-24 09:41:46 -0500

    add

--- src/etl_reporte_llamadas.py
+++ src/etl_reporte_llamadas.py
@@ -4,41 +4,39 @@
 #   reporte = generate_report(datos)
 #   save_data(reporte)
 
-from argparse import RawDescriptionHelpFormatter
 import os
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 
-root_dir = Path (".")
+root_dir = Path('.').resolve()
+
 
 def get_data(filename):
-
-    filename = 'llamadas123_julio_2022.csv'
-    root_dir = Path('.').resolve()
     data_dir = 'raw'
-    datos = get_data(filename)
-    file_path = os.path.join(root_dir, 'data',data_dir,filename)
-    
+    file_path = os.path.join(root_dir, 'data', data_dir, filename)
     datos = pd.read_csv(file_path, sep=';', encoding='latin-1')
+
     print('get_data')
-    print('la tabla contiene', datos.shape[0], 'filas',datos.shape[1],'columnas')
+    print('La tabla contiene', datos.shape[0],
+          'filas', datos.shape[1], 'columnas')
     return datos
 
+
 def generate_report(datos):
-    #Crear diccionario vacio
     dict_resumen = dict()
 
     for col in datos.columns:
-    valores_unicos =datos [col].unique()
-    n_valores = len(valores_unicos)
-    dict_resumen[col] = n_valores
+        valores_unicos = datos[col].unique()
+        n_valores = len(valores_unicos)
+        dict_resumen[col] = n_valores
 
     reporte = pd.DataFrame.from_dict(dict_resumen, orient='index')
-    reporte.rename({0:'conteo'},axis=1,inplace=True)
+    reporte.rename({0: 'Conteo'}, axis=1, inplace=True)
 
-    print('generate_reprt')
-    print(report.head)
+    print('generate_report')
+    print(reporte.head())
     return reporte
+
 
 def save_data(reporte, filename, step='resumen'):
 
@@ -48,11 +46,11 @@ def save_data(reporte, filename, step='resumen'):
 
 
 def main():
+    filename = 'llamadas123_julio_2022.csv'
+    datos = get_data(filename)
+    reporte = generate_report(datos)
+    save_data(reporte, filename)
 
-    filename = "llamadas123_julio_2022.csv"
-    datos = get_data (filename)
-    reporte = generate_report (datos)
-    save_data(reporte,filename)
 
 if __name__ == '__main__':
     main()
